546ceac8126a5190fb059f0c0c2add582fbb0ad9
starboyy1 <tayyabmansoor789@gmail.com>
2024-05-03 18:49:45 +0500

    First Commit

---
+++ f.py
@@ -0,0 +1,32 @@
+"""i=1
+while i<=100:
+    if i%2==0:
+       print("num is even ",i)
+    i +=1  
+    if i%2!=0:
+        print("num is odd ",i) 
+else:
+    print("odd")     """ 
+"""i=(int(input("enter a number")))
+for n in range(2,i):
+    if i%n==0:
+      print("num is not prime",i)
+    else:
+     print("prime number",i)"""
+"""n=(input())
+m={'a','e','i','o','u'}
+if n in  m:
+    print("error")
+else:
+    print(n)"""
+"""n=1
+sum=0
+while(n<=100):
+    if n%2!=0:
+        sum+=n
+    n += 1
+else:
+    n += 1
+print(sum)"""
+
+        
